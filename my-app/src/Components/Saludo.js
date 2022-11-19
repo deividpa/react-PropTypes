48949c3d55f7c7ec75ae6bfbcee0ee97fad93fd5
David Pérez <deividpa022@gmail.com>
2022-11-18 23:39:55 -0500

    Se hace uso de la propiedad de React, defaultProps para establecer propiedades por defecto que podrían no haber sido declaradas desde el componente padre

--- my-app/src/Components/Saludo.js
+++ my-app/src/Components/Saludo.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function Saludo({ nombre }) {
+export default function Saludo({ nombre, apellido }) {
   return <h1>Hola, este es un saludo de parte de {nombre}</h1>;
 }
 
 Saludo.propTypes = {
   nombre: PropTypes.string.isRequired,
+};
+
+Saludo.defaultProps = {
+  apellido: "Soy un apellido por defecto",
 };
